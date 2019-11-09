7c2de69ef7dcb3209c7f76fe9a6406d1b816fa0b
Darcy Shen <sadhen@zoho.com>
2019-11-09 17:49:19 +0800

    Sync

--- tests/Kernel/Containers/list_test.cpp
+++ tests/Kernel/Containers/list_test.cpp
@@ -1,3 +1,4 @@
+
 /******************************************************************************
 * MODULE     : list_test.cpp
 * DESCRIPTION: test on linked lists with reference counting
@@ -7,10 +8,10 @@
 * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
 * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
 ******************************************************************************/
+
 #include "gtest/gtest.h"
 
 #include "list.hpp"
-#include "string.hpp"
 
 static list<long> gen(int64_t n) {
   auto normal = list<long>();
